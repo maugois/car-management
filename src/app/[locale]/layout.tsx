ecb924134204396da81e72aeee87d1461c223280
maugois <gois.mauricio12@gmail.com>
2026-02-18 13:25:25 -0300

    feat: initial project

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
+import {getTranslations} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
+import "./globals.css";
+
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{locale: string}>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const {locale} = await params;
+  const t = await getTranslations({ locale, namespace: 'metadata' });
+
+  return {
+    title: t('title'),
+    description: t('description'),
+  };
+}
+
+export default async function RootLayout({ children, params, }: Readonly<{ children: React.ReactNode; params: Promise<{locale: string}> }>) {
+  const {locale} = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale}>
+      <body className={`antialiased`}>
+        <NextIntlClientProvider locale={locale}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/i18n/routing.ts
@@ -0,0 +1,16 @@
+import {defineRouting} from 'next-intl/routing';
+ 
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales: ['en', 'pt'],
+ 
+  pathnames: {
+    home: {
+      en: '/',
+      pt: '/',
+    },
+  },
+
+  // Used when no locale matches
+  defaultLocale: 'en'
+});
